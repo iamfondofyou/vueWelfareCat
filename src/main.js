c70cd08a68db57e81aa478c73fb58d30ac70b07b
vinieo <374863506@qq.com>
2018-08-13 12:22:57 +0800

    unfinished

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store/index';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   router,
-  store,
-  render: h => h(App),
-}).$mount('#app');
+  components: { App },
+  template: '<App/>'
+})
--- src/store/mutation-types.js
+++
@@ -1,3 +0,0 @@
-export const USER_INFO = 'USER_INFO';
-export const USER_MESSAGE = 'USER_MESSAGE';
-
